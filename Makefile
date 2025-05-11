51bada38b62bb1c6d2d8feac999cca43a3aaf0cd
Anton <poverenosn@gmail.com>
2025-05-11 18:17:07 +0300

    step1

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 install:
-	uv sync
+	curl -LsSf https://astral.sh/uv/install.sh | sh
+	source $HOME/.local/bin/env && uv pip install -r requirements.txt
 
 git:
 	git add .
--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
-# здесь добавьте все необходимые команды для установки вашего проекта
-# команду установки зависимостей, сборки статики, применения миграций и другие
-make install && make collectstatic && make migrate
+
+uv pip install -r requirements.txt
+python manage.py collectstatic --noinput
+python manage.py migrate

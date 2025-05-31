8f208a2cba657142f7266803d2c549d7403e613e
Anton <poverenosn@gmail.com>
2025-05-31 18:00:14 +0300

    gf

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -99,9 +99,9 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 # settings.py
 DATABASES = {
-    'default': dj_database_url.parse(
-        'postgresql://postgres:1234@localhost:5432/task_manager',
-        conn_max_age=600
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        default='postgresql://postgres:1234@localhost:5432/task_manager'
     )
 }
 

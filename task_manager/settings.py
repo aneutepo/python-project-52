c88f3bc99af6753a351762c2540b291c6d682e4e
Anton <poverenosn@gmail.com>
2025-05-31 17:53:47 +0300

    gf

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -97,11 +97,12 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# settings.py
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        'postgresql://postgres:1234@localhost:5432/task_manager',
+        conn_max_age=600
+    )
 }
 
 db_from_env = dj_database_url.config(conn_max_age=600)
